0cd5cbc54b9e989758aee3b8a63724f70cfafa3a
jovanna1616 <jovana.r@vivifyideas.com>
2020-07-13 10:51:09 +0200

    Added basic routing and project templates structure.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("HOMEPAGE")
+    return render(request=request,
+                  template_name="main/home.html")
+
+def login_request(request):
+    return render(request=request,
+                  template_name="main/auth/login.html")
+
+def register_request(request):
+    return render(request=request,
+                  template_name="main/auth/register.html")
+
+def logout_request(request):
+    return render(request=request,
+                  template_name="main/auth/logout.html")
